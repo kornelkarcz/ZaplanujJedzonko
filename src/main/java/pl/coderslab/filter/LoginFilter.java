80dfd2f67ba56b18c3cc0422087a109e86a71ced
kornelkarcz <kornel.karcz@outlook.com>
2019-03-11 13:39:53 +0100

    Cleaning up

--- src/main/java/pl/coderslab/filter/LoginFilter.java
+++ src/main/java/pl/coderslab/filter/LoginFilter.java
@@ -1,4 +1,35 @@
 package pl.coderslab.filter;
 
-public class LoginFilter {
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("/app/*")
+public class LoginFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        HttpSession session = request.getSession(false);
+
+        boolean loggedIn = (session != null) && (session.getAttribute("currentUser") != null);
+
+        if (loggedIn) {
+            chain.doFilter(request, response);
+        } else {
+            response.sendRedirect("/login");
+        }
+
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
 }
+
